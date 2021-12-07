9c129ded440359f24d4441b6e49d6d8ef178d1be
Nahom Fikadu <nahi.terefe@gmail.com>
2021-12-08 00:07:03 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-
 class Square:
-    """Class defined here as empty class of square"""
     def __init__(self, size=0):
-        if isinstance(size, int):
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
-        elif not isinstance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
