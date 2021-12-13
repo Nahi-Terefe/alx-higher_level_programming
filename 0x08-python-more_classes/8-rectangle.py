997061c62bf81203ffac865f76efe5d514842f9f
Nahom Fikadu <nahi.terefe@gmail.com>
2021-12-14 01:02:09 +0300

    Update 8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle:
         """ Initialize the class"""
         self.width = width
         self.height = height
-        type(self).number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
