9f53df3f2495a18af55cd28a824ce6adb2895369
Nahom Fikadu <nahi.terefe@gmail.com>
2021-12-20 21:16:10 +0300

    Create 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" A lookup module"""
+
+def lookup(obj):
+    """" A function that returns the list of
+         available method and attribute a given
+         object have """
+
+    return dir(obj)
