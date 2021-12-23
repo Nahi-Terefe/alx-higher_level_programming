fa3b3fe6dec0a4a596de9f67d84bd26d8531e613
Nahom Fikadu <nahi.terefe@gmail.com>
2021-12-23 07:47:54 +0300

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -14,6 +14,18 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """dictionary"""
-        return self.__dict__
+        if attrs is not None and all(isinstance(item, str) for item in attrs):
+            ret = {}
+            for p, r in self.__dict__.items():
+                if p in attrs:
+                    ret[p] = r
+            return ret
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """change all attributes"""
+        for p, r in json.items():
+            self.__dict__[p] = r
