d2f7d7fc2fac408f3a20c7a73a35670eeb8210c9
Nahom Fikadu <nahi.terefe@gmail.com>
2021-12-23 07:59:19 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,20 @@
+
 #!/usr/bin/python3
+"""Add item script."""
 import sys
-savejson = __import__("7-save_to_json_file").save_to_json_file
-loadjson = __import__("8-load_from_json_file").load_from_json_file
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-"""Description of module."""
-
-
-oldlist = []
 try:
-    oldlist = loadjson("add_item.json")
+    lst = load_from_json_file("add_item.json")
 except:
-    pass
-savejson(oldlist + sys.argv[1:], "add_item.json")
+    lst = []
+
+argc = len(sys.argv)
+
+if argc > 1:
+    for i in range(1, argc):
+        lst.append(sys.argv[i])
+
+save_to_json_file(lst, "add_item.json")
