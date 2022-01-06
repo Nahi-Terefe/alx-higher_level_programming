c0ba584b69646ed72d8e74bbc6196094331774af
Nahom Fikadu <nahi.terefe@gmail.com>
2022-01-06 21:13:25 +0300

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -38,35 +38,39 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """ width setter """
-        if type(width) != int:
+        if type(width) is not int:
             raise TypeError("width must be an integer")
-        if width <= 0:
+        elif width <= 0:
             raise ValueError("width must be > 0")
-        self.__width = width
+        else:
+            self.__width = width
 
     @height.setter
     def height(self, height):
         """ height setter """
-        if type(height) != int:
+        if type(height) is not int:
             raise TypeError("height must be an integer")
-        if height <= 0:
+        elif height <= 0:
             raise ValueError("height must be > 0")
-        self.__height = height
+        else:
+            self.__height = height
 
     @x.setter
     def x(self, x):
         """ x setter """
-        if type(x) != int:
+        if type(x) is not int:
             raise TypeError("x must be an integer")
-        if x < 0:
+        elif x < 0:
             raise ValueError("x must be >= 0")
-        self.__x = x
+        else:
+            self.__x = x
 
     @y.setter
     def y(self, y):
         """ y setter """
-        if type(y) != int:
+        if type(y) is not int:
             raise TypeError("y must be integer")
-        if y < 0:
+        elif y < 0:
             raise ValueError("y must be >= 0")
-        self.__y = y
+        else:
+            self.__y = y
