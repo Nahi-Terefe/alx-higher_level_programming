16cda9182b8f39a7003bcc23f040f56e1425f29e
Nahom Fikadu <nahi.terefe@gmail.com>
2022-01-06 20:47:05 +0300

    Create rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+""" A rectangle module """
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ A rectangle class """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """Getter"""
+        return self.__width
+
+    @property
+    def height(self):
+        """Getter"""
+        return self.__height
+
+    @property
+    def x(self):
+        """Getter"""
+        return self.__x
+
+    @property
+    def y(self):
+        """Getter"""
+        return self.__y
+
+    @width.setter
+    def width(self, width):
+        """ width setter """
+        self.__width = width
+
+    @height.setter
+    def height(self, height):
+        """ height setter """
+        self.__height = height
+
+    @x.setter
+    def x(self, x):
+        """ x setter """
+        self.__x = x
+
+    @y.setter
+    def y(self):
+        """ y setter """
+        self.__y = y
