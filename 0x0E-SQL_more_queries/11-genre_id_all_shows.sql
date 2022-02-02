84ec902f963d00cdbac95035dea27f394bfefe4b
Nahom Fikadu <nahi.terefe@gmail.com>
2022-02-03 02:03:26 +0300

    Create 11-genre_id_all_shows.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,3 @@
+-- list genre id for all shows
+-- display tv show title and genre id
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
