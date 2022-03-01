61349320d763d79d4e45c6d247027196900906bb
Nahom Fikadu <nahi.terefe@gmail.com>
2022-03-01 19:19:25 +0300

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
+'use strict';
 
-if (process.argv.length <= 2) {
+let argvEelement = process.argv.length;
+if (argvEelement <= 2) {
     console.log('No argument')
-}else if(process.argv.length === 3) {
+}else if(argvEelement === 3) {
     console.log('Argument found')
-}else if(process.argv.length > 3) {
+}else if(argvEelement > 3) {
     console.log('Arguments found')
 }
