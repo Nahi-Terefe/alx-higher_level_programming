e2ebda4f0a9364de10584516dacb4d5fa5eab4f9
Nahom Fikadu <nahi.terefe@gmail.com>
2022-03-01 19:15:53 +0300

    Add 2-arguments.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const cIsFun = 'C is fun';
-const pythonIsCool = "Python is cool";
-const javaScriptIsAmazing = 'JavaScript is amazing';
-console.log(cIsFun + '\n' + 
-            pythonIsCool + '\n' + 
-            javaScriptIsAmazing);
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+console.log(myVar.join('\n'));
