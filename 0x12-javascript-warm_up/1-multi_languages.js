c3aacf192dea183ab4fd1d64fbdbc21b852c0088
Nahom Fikadu <nahi.terefe@gmail.com>
2022-03-01 19:27:40 +0300

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
 // logs content of all varibale
 
-const cIsFun = 'C is fun';
-const pythonIsCool = "Python is cool";
-const javaScriptIsAmazing = 'JavaScript is amazing';
-console.log(cIsFun + '\n' + 
-            pythonIsCool + '\n' + 
-            javaScriptIsAmazing);
+console.log("C is fun\nPython is cool\nJavaScript is amazing")
