d56967011319613de48aaf6d03808d39bd53f4fe
Nahom Fikadu <nahi.terefe@gmail.com>
2022-03-01 18:31:14 +0300

    Update file permission

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+'use strict';
+
+const cIsFun = 'C is fun';
+const pythonIsCool = "Python is cool";
+const javaScriptIsAmazing = 'JavaScript is amazing';
+
+console.log(cIsFun + '\n' + pythonIsCool + '\n' + javaScriptIsAmazing);
