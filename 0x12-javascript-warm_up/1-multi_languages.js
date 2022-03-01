496a84042ce2f8fe8e4f359aed525c6e4829d317
Nahom Fikadu <nahi.terefe@gmail.com>
2022-03-01 18:33:14 +0300

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -4,5 +4,4 @@
 const cIsFun = 'C is fun';
 const pythonIsCool = "Python is cool";
 const javaScriptIsAmazing = 'JavaScript is amazing';
-
 console.log(cIsFun + '\n' + pythonIsCool + '\n' + javaScriptIsAmazing);
