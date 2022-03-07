46a5db6ff394347e018d1e81d11f31c47f748985
Nahom Fikadu <nahi.terefe@gmail.com>
2022-03-07 22:42:49 +0300

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -21,7 +21,8 @@ if __name__ == "__main__":
     cursor = database.cursor()
 
     cursor.execute("""SELECT cities.id, cities.name, states.name
-                   FROM cities JOIN states
+                   FROM cities
+                   JOIN states
                    ON cities.state_id = states.id
                    ORDER BY id ASC;""")
 
