fd0220b1a58cdf9e507ca6f00000abd9643701f3
Nahom Fikadu <nahi.terefe@gmail.com>
2022-03-07 19:59:31 +0300

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,45 +1,21 @@
 #!/usr/bin/python3
-""" Select all state from database and diplay it."""
-from sqlalchemy import create_engine, Table, Integer, String, Column, select
-from sqlalchemy.orm import sessionmaker, declarative_base
+"""Conect DataBase and query"""
+
+import MySQLdb
 import sys
-import pymysql
-
-Base = declarative_base()
-
-username = sys.argv[1]
-password = sys.argv[2]
-dbName = sys.argv[3]
-host = 'localhost'
-port = 3306
-
-
-class states(Base):
-    """states model definition or their schema"""
-
-    __tablename__ = "states"
-
-    id = Column(Integer, primary_key=True, autoincrement="auto")
-    name = Column(String(255), nullable=False)
-
-    def __repr__(self):
-        return "<states %r>" % self.username
-
 
 if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
 
-    db_url = "mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(
-             username, password, host, port, dbName)
-    # create database engine / connect to the database
-    db_engine = create_engine(db_url)
-
-    connection = db_engine.connect()
-    connection = connection.execution_options(
-        isolation_level="READ COMMITTED"
-    )
-
-    s = select([states])
-    result = connection.execute(s)
-
-    for row in result:
+    # Execute the query
+    cur.execute("SELECT id, name FROM states ORDER BY states.id ASC;")
+    for row in cur.fetchall():
         print(row)
+
+    cur.close()
+    db.close()
