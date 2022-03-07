28a6484be374640db2ac39601322b2a5ae6bc722
Nahom Fikadu <nahi.terefe@gmail.com>
2022-03-07 23:29:00 +0300

    Create 9-model_state_filter.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Lists all State objects"""
 
-from .model_state import Base, State
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sys
