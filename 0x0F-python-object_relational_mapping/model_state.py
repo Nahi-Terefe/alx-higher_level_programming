ac4162d8377e54e57d48d0bfcbf95841e174cf82
Nahom Fikadu <nahi.terefe@gmail.com>
2022-03-07 23:31:23 +0300

    Create 9-model_state_filter.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
-Write a python file that contains the class definition
-of a State and an instance Base = declarative_base()
+link the class with the database
 """
+
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 
 class State(Base):
-    """ Define a class State to be linked to db table """
+    """State class."""
     __tablename__ = 'states'
 
-    id = Column(Integer, nullable=False,
-                autoincrement=True, unique=True,
-                primary_key=True)
-
+    id = Column(Integer,
+                primary_key=True,
+                nullable=False,
+                autoincrement="auto")
     name = Column(String(128), nullable=False)
