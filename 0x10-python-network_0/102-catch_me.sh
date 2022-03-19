bdca5bae12eea0f1d62c6a534f592795ecff3082
Nahom Fikadu <nahi.terefe@gmail.com>
2022-03-20 00:24:25 +0300

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# Script to curl catch_me file and display
-curl -s -H --data @catch_me 0.0.0.0:5000/catch_me
+# script that makes a request to 0.0.0.0:5000/catch_me that 
+# causes the server to respond with a message containing You got me!, 
+# in the body of the response.
+curl -sX PUT -L -d "user_id=98" --header "origin: alxschool" 0.0.0.0:5000/catch_me
