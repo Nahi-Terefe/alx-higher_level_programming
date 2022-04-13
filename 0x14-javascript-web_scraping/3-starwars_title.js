e59c07dc1b4ab19487d144c02288e42054bdb984
Nahom Fikadu <nahi.terefe@gmail.com>
2022-04-13 05:33:45 +0300

    Create 5-request_store.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 'use strict';
 const request = require('request');
 
-request('http://swapi.co/api/films/' + process.argv[2], { json: true }, function (e, r, b) {
+request('https://swapi-api.hbtn.io/api/films/:id' + process.argv[2], { json: true }, function (e, r, b) {
   if (e) console.log(e); else {
     if (r.statusCode === 200) console.log(b.title);
   }
